31760f0fde84b5d683390090f8a917fc6e2d5581
esiolima <132793812+esiolima@users.noreply.github.com>
2026-02-12 08:19:54 -0300

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -3,11 +3,13 @@ import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, router } from "./_core/trpc";
 import { cardRouter } from "./routers/cardRouter";
+import { logoRouter } from "./routers/logoRouter";
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
   system: systemRouter,
   card: cardRouter,
+  logo: logoRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
     logout: publicProcedure.mutation(({ ctx }) => {
